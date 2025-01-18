89e750e0ed8f839110ac7f85935c6af906c54601
sik2 <bickwang123@gmail.com>
2025-01-19 04:34:57 +0900

    init

---
+++ src/main/java/com/ll/es/SbEs2501ExApplication.java
@@ -0,0 +1,13 @@
+package com.ll.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEs2501ExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbEs2501ExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/es/SbEs2501ExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEs2501ExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
