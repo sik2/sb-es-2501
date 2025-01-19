cca0a2ba83a772ab26978f8f048f78e8bb77be82
sik2 <bickwang123@gmail.com>
2025-01-20 00:45:22 +0900

    fix compose, add nori

--- src/main/java/com/ll/es/domain/post/postDoc/document/PostDoc.java
+++ src/main/java/com/ll/es/domain/post/postDoc/document/PostDoc.java
@@ -7,8 +7,12 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+import org.springframework.data.elasticsearch.annotations.Mapping;
 
 @Document(indexName = "app1_posts")
+@Setting(settingPath = "elasticsearch/post-setting.json")
+@Mapping(mappingPath = "elasticsearch/post-mapping.json")
 @Getter
 @Setter
 @Builder
